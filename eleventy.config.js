5cc8ed4f1eeb1ca70aa9a983e1c676da36112075
Steven Spriggs <steven.spriggs@gmail.com>
2025-09-17 15:32:19 -0400

    add eleventy config missing due to gitignore rules

---
+++ eleventy.config.js
@@ -0,0 +1,16 @@
+import { Liquid } from "liquidjs";
+
+export default async function(eleventyConfig) {
+	eleventyConfig.setInputDirectory("docs");
+	eleventyConfig.setOutputDirectory("_site");
+
+  const options = {
+    jsTruthy: true,
+    dynamicPartials: true,
+    strictFilters: false,
+    extensions: [".liquid, .html"],
+    root: ["./docs"],
+  };
+
+  eleventyConfig.setLibrary("liquid", new Liquid(options));
+};
